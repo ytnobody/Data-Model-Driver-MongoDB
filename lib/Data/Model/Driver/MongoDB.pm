ed1372e96f457dd38bb3ac34034be2d38966cc17
ytnobody <ytnobody@gmail.com>
2011-05-02 11:50:56 +0900

    initial commit

---
+++ lib/Data/Model/Driver/MongoDB.pm
@@ -0,0 +1,32 @@
+package Data::Model::Driver::MongoDB;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Data::Model::Driver::MongoDB -
+
+=head1 SYNOPSIS
+
+  use Data::Model::Driver::MongoDB;
+
+=head1 DESCRIPTION
+
+Data::Model::Driver::MongoDB is
+
+=head1 AUTHOR
+
+ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
